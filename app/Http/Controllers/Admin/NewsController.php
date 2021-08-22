16857987f2afcc945d6cff313fc133c1bf179865
河田将太 <shota.kawata1104@gmail.com>
2021-08-22 06:06:33 +0000

    php23_work

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+//以下を追記することでNews Modelを使えるようになる
+use App\News;
+
 class NewsController extends Controller
 {
     //
@@ -16,6 +19,31 @@ class NewsController extends Controller
 
     public function create(Request $request)
     {
+        
+        //validationを行う
+        $this->validate($request, News::$rules);
+        
+        $news = new News;
+        $form = $request->all();
+        
+        //フォームから画像が送信されて来たら、保存して、$news->image_path に画像のパスを保存する
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $news->image_path = basename($path);
+        } else{
+            $news->image_path = null;
+        }
+        
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        //フォームから送信されてきたimageをさくじょする
+        unset($form['image']);
+        
+        //データベースに保存する
+        $news->fill($form);
+        $news->save();
+        
+        
         return redirect('admin/news/create');
     }
     
