eb1450d8e861d14effe50a2a1b0abe329b870aea
河田将太 <shota.kawata1104@gmail.com>
2021-09-04 21:13:44 +0000

    bucket_test

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -11,7 +11,7 @@
                             <div class="caption mx-auto">
                                 <div class="image">
                                     @if ($headline->image_path)
-                                        <img src="{{ asset('storage/image/' . $headline->image_path) }}">
+                                        <img src="{{ $headline->image_path }}">
                                     @endif
                                 </div>
                                 <div class="title p-2">
@@ -45,7 +45,7 @@
                             </div>
                             <div class="image col-md-6 text-right mt-4">
                                 @if ($post->image_path)
-                                    <img src="{{ asset('storage/image/' . $post->image_path) }}">
+                                    <img src="{{ $post->image_path }}">
                                 @endif
                             </div>
                         </div>
@@ -53,7 +53,6 @@
                     <hr color="#c0c0c0">
                 @endforeach
             </div>
-            <p>課題1</p>
         </div>
     </div>
     </div>
